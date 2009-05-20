e3eb0e1cd5eda88e472c28f01b174e712a7c8408
Дмитрий <pronix.service@gmail.com>
2009-05-20 15:19:05 +0400

    tourist edit check

--- app/controllers/tourist_controller.rb
+++ app/controllers/tourist_controller.rb
@@ -1,31 +1,37 @@
 class TouristController < ApplicationController
   def index
-    #@tourists = Tourist.find(:all)
-    # params[:page] ||= 1
-    # @tourists = Tourist.paginate :page => params[:page], :order => 'id DESC'
-
     @tourists_grid = initialize_grid(Tourist,
                                     :per_page => 20,
                                     :order => 'created_at',
                                     :order_direction => 'desc',
                                     :include => :user
                                     )
-
   end
 
   def new
-
   end
 
   def edit
+    @tourist = Tourist.find_by_id(params[:id])
+    render :file => 'app/views/tourist/new.html.erb', :layout => 'menu'
   end
 
   def save
-    # newt = newtourist
-    @newt = Tourist.new(params[:tourist])
-    @newt.user_id = session[:user]
-    @newt.save!
-    redirect_to :action => 'view', :id => @newt.id
+    if params[:id].empty?
+      @newt = Tourist.new(params[:tourist])
+      @newt.user_id = session[:user]
+    else
+      @newt = Tourist.find_by_id(params[:id])
+      params['tourist'].each do |key,val|
+       @newt[key] = val
+      end
+    end
+    if @newt.save!
+      flash[:notice] = 'Сохранен турист'
+    else
+      flash[:error] = 'Произошла ошибка при сохранении туриста'
+    end
+    redirect_to :action => 'index', :id => @newt.id
   end
 
   def view
