b7ac75da8c37d59d4e373ced36f5199445538ca9
Дмитрий <pronix.service@gmail.com>
2009-06-10 11:19:54 +0400

    добавлено в турагента в документай "в лице директора"

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ActiveRecord::Base
   def rekvizit
-    "#{self.name},\n юридический адрес #{self.yur_adress}, \n физический адрес #{self.fiz_adress},\nтелефон #{self.phone}, ОГРН #{self.ogrn}, ИНН #{self.inn}, КПП #{self.kpp},\n расчетный счет #{self.r_s4},\n #{self.bank},\n БИК #{self.bik} "  
+    "#{self.name},в лице генерального директора #{self.director}\n юридический адрес #{self.yur_adress}, \n физический адрес #{self.fiz_adress},\nтелефон #{self.phone}, ОГРН #{self.ogrn}, ИНН #{self.inn}, КПП #{self.kpp},\n расчетный счет #{self.r_s4},\n #{self.bank},\n БИК #{self.bik} "  
   end
 end
