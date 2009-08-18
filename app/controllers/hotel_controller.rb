b6c48b154da08abde28686d639a12808f99651d7
pronix <pronix.service@gmail.com>
2009-08-18 13:55:23 +0400

    add check city for hotel and error report if it not exist

--- app/controllers/hotel_controller.rb
+++ app/controllers/hotel_controller.rb
@@ -14,10 +14,14 @@ class HotelController < ApplicationController
   end
 
   def save
+    unless params[:city][:name].empty?
     @city = City.find_by_name(params[:city][:name])
     params['hotel']['city_id'] = @city.id
     params['hotel']['country_id'] = @city.country_id
     @hotel = Hotel.create(params['hotel'])
+    else
+      flash[:error] = 'Необходимо указать город в котором находится гостиница'
+    end
     redirect_to :action => 'index'
 
   end
